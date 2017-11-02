e839d499de259261da0886eea89c95148cd92de0
Josh Ting <joshting25@gmail.com>
2017-11-02 16:40:33 -0400

    Add stdscr boilerplate

--- jr.py
+++ jr.py
@@ -7,11 +7,14 @@ length = 30
 width = 10
 
 def init():
+    stdscr = curses.initscr()
     curses.noecho()    # don't echo the keys on the screen
     curses.cbreak()    # don't wait enter for input
+    stdscr.keypad(True)
     window = curses.newwin(length, width, 0, 0)  # create a 30x10 window
     window.box()       # Draw the box outside the window
-    return window
+    stdscr.clear()
+    return window, stdscr
 
 def destruct(window, stdscr):
     curses.nocbreak()
@@ -21,24 +24,27 @@ def destruct(window, stdscr):
 
 def parseFile(cur_file):
     result = []
-
-
+    for line in cur_file:
+        result.append(line)
+    return result
 
 def main(stdscr):
-    window = init()
+    window, stdscr = init()
 
-    frame = []
-    filenames = glob.glob("art/*.txt")
+    frames = []
+    filenames = glob.glob("2017/*.txt")
     for filename in filenames:
         cur_file = open(filename, "r")
-        frame.append(parseFile(cur_file))
+        frames.append(parseFile(cur_file))
 
-    for row in range(len(frame)):
-        for col in range(len(frame[row])):
-            stdscr.addch(row, col, frame[row][col])
+    for row in range(len(frames)):
+        for col in range(len(frames[row])):
+            stdscr.addstr(row, col, (frames[row][col]))
             stdscr.refresh()
         time.sleep(0.2)
 
     destruct(window, stdscr)
 
 wrapper(main)
+# if __name__ == "__main__":
+#     main()
