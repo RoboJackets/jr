d63c999eeccca9ccf72acb3b49369fba6de17f7b
Jason Gibson <jgibson37@gatech.edu>
2017-11-03 19:03:49 -0400

    working version of jr

--- jr.py
+++ jr.py
@@ -3,15 +3,15 @@ import curses
 import time
 from curses import wrapper
 
-length = 30
-width = 10
+length = 80
+width = 24
 
 def init():
     stdscr = curses.initscr()
     curses.noecho()    # don't echo the keys on the screen
     curses.cbreak()    # don't wait enter for input
     stdscr.keypad(True)
-    window = curses.newwin(length, width, 0, 0)  # create a 30x10 window
+    window = curses.newwin(length, width, 0, 0)  # create a window
     window.box()       # Draw the box outside the window
     stdscr.clear()
     return window, stdscr
@@ -25,7 +25,12 @@ def destruct(window, stdscr):
 def parseFile(cur_file):
     result = []
     for line in cur_file:
+        if len(line) < length:
+            line += " " * (length - len(line))
         result.append(line)
+    if len(result) < width:
+        for i in range(width - len(result)):
+            result.append(" " * length)
     return result
 
 def main(stdscr):
@@ -33,18 +38,22 @@ def main(stdscr):
 
     frames = []
     filenames = glob.glob("2017/*.txt")
+    black = []
+    for i in range(width):
+        black.append(" " * length)
+    frames.append(black)
+    frames.append(parseFile(open("required/ship.txt", "r")))
     for filename in filenames:
         cur_file = open(filename, "r")
         frames.append(parseFile(cur_file))
-
-    for row in range(len(frames)):
-        for col in range(len(frames[row])):
-            stdscr.addstr(row, col, (frames[row][col]))
-            stdscr.refresh()
-        time.sleep(0.2)
-
+    frames.append(parseFile(open("required/jason.txt", "r")))
+    for start_col in range((len(frames) - 1) * length):
+        for col in range(length):
+            for row in range(width):
+                stdscr.addch(row, col, (frames[(start_col + col) / length][row][(start_col + col) % length]))
+        stdscr.refresh()
+        time.sleep(0.05)
     destruct(window, stdscr)
 
-wrapper(main)
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    wrapper(main)
